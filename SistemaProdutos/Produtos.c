ab8c453705cf2a2e2878bab5e65c87ab1482b2a5
JulioCesra <Juliocesar2006n@hotmail.com>
2025-05-27 20:47:58 -0300

    Criação das Structs Item e ListaCompras!

--- SistemaProdutos/Produtos.c
+++ SistemaProdutos/Produtos.c
@@ -3,6 +3,18 @@
 #include <string.h>
 
 
+struct Item{
+    char nome[30];
+    float valorUnitario;
+    int quantidade;
+
+};
+
+struct ListaCompras{
+    float valorTotal;
+    int capacidadeEstoque;
+};
+
 int main(void){
 
 
